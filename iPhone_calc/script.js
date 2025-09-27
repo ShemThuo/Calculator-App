f930f40cf8b3eb4f7c42a9dacf97a673a963114c
Techflux <shemtmwangi@gmail.com>
2025-09-27 11:46:01 +0300

    Error found

--- iPhone_calc/script.js
+++ iPhone_calc/script.js
@@ -16,10 +16,12 @@
         const maxlen = 9;
 
         if (
-            /e/i.test(val)  || val.replace("-", "").replace(".", "").length <= maxlen
-        ) { display.textContent = val; }
+            /e/i.test(value)  || value.replace("-", "").replace(".", "").length <= maxlen
+        ) { 
+            display.textContent = value; 
+        }
         else {
-            const num = Number(val);
+            const num = Number(value);
             display.textContent = num.toExponential(6).replace("+", "");
         }
     }
@@ -49,10 +51,38 @@
             state.displayValue = state.displayValue === "0" ? d : state.displayValue + d;
         }
         clearOpHighlights();
-        setACLabel("C");
+        setACLabel('C');
         updateDisplay();
     }
 
+    function inputDecimal() {
+        if (state.waitingForSecondOperand) {
+            state.displayValue = "0.";
+            state.waitingForSecondOperand = false;
+        } else  {
+            state.displayValue += ".";
+        }
+        setACLabel('C');
+        updateDisplay();
+    }
+
+    function toggleSign() {
+        if (state.displayValue === "0") return;
+        state.displayValue = String(-Number(state.displayValue));
+        updateDisplay
+    }
+
+    function percent() {
+        let current = Number(state.displayValue);
+        if(state.firstOperand !== null && state.operator && !state.waitingForSecondOperand) {
+            current = state.firstOperand * (current / 100);
+        }else {
+            current = current / 100;
+        }
+        state.displayValue = String(current);
+        updateDisplay();
+    }
+    
     keys.addEventListener("click", (e) => {
         const t = e.target.closest("button");
         if (!t) return;
@@ -60,5 +90,18 @@
             inputDigit(t.dataset.digit);
             return;
         }
+
+        const action = t.dataset.action
+
+        switch (action) {
+            case "decimal":
+                inputDecimal();
+                break;
+            case "sign":
+                toggleSign();
+                break;
+            default:
+                break;
+        }
     });
-})();
+});
